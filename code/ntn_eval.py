b42741a38c74ec15b8a7d4cb240b453222867ac0
Chief <luyolo.nqobile@gmail.com>
2018-06-06 06:36:33 +0200

    Cross Entropy Loss for Semantic Reasoning
    - Move entity batch collection outside ntn computation loop
    - Set learning rate to 1 and l2_lambda to 1e-3
    - Add computed and target relationship for every entity pair as logit and target vectors
    - Use logit and target vectors to compute softmax cross entropy loss
    - update evaluation data loading process to accomodate logit and target entity vectors
    - load saved model parameters to evaluate ntn model
    - set batch size to 10000
    - Model validation
    - Full data set training and validation
    - Accuracy based, validation early stopping
    - Testing data set

--- code/ntn_eval.py
+++ code/ntn_eval.py
@@ -6,27 +6,28 @@ import tensorflow as tf
 def data_to_indexed(data, entities, relations):
     entity_to_index = {entities[i] : i for i in range(len(entities))}
     relation_to_index = {relations[i] : i for i in range(len(relations))}
-    indexed_data = [(entity_to_index[data[i][0]], relation_to_index[data[i][1]],\
+    indexed_data = [(entity_to_index[data[i][0]], relation_to_index[data[i][1]], \
             entity_to_index[data[i][2]], float(data[i][3])) for i in range(len(data))]
     return indexed_data
 
-def fill_feed_dict(batches, labels, train_both, batch_placeholders, label_placeholders, corrupt_placeholder):
-    feed_dict = {corrupt_placeholder: [train_both and np.random.random()>0.5]}
-    for i in range(len(batch_placeholders)):
-        feed_dict[batch_placeholders[i]] = batches[i]
-    for i in range(len(label_placeholders)):
-        feed_dict[label_placeholders[i]] = labels[i]
+def fill_feed_dict(batches, train_both, batch_placeholders, label_placeholders, corrupt_placeholder):
+    feed_dict = {corrupt_placeholder: [train_both and np.random.random() > 0.5]}
+    # for i in range(len(batch_placeholders)):
+    #     feed_dict[batch_placeholders[i]] = batches[i]
+    feed_dict[batch_placeholders] = batches
+    # for i in range(len(label_placeholders)):
+    #     feed_dict[label_placeholders[i]] = labels[i]
     return feed_dict
-
 
 #dataset is in the form (e1, R, e2, label)
 def data_to_relation_sets(data_batch, num_relations):
-    batches = [[] for i in range(num_relations)]
+    # batches = [[] for i in range(num_relations)]
+    batches = []
     labels = [[] for i in range(num_relations)]
-    for e1,r,e2,label in data_batch:
-        batches[r].append((e1,e2,1))
-        labels[r].append([label])
-    return (batches, labels)
+    for e1, r, e2, label in data_batch:
+        batches.append((e1, e2, 1, r))
+        labels.append(label)
+    return batches, labels
 
 def run_evaluation():
     print(params.output_path)
@@ -41,39 +42,56 @@ def run_evaluation():
     num_relations = len(relations_list)
 
     slice_size = params.slice_size
-    (init_word_embeds, entity_to_wordvec) = ntn_input.load_init_embeds(params.data_path)
+    init_word_embeds, entity_to_wordvec = ntn_input.load_init_embeds(params.data_path)
     batches, labels = data_to_relation_sets(test_data, num_relations)
-
 
     with tf.Graph().as_default():
         sess = tf.Session()
-        batch_placeholders = [tf.placeholder(tf.float32, shape=(None, 3)) for i in range(num_relations)]
+        # batch_placeholders = [tf.placeholder(tf.float32, shape=(None, 3)) for i in range(num_relations)]
+        batch_placeholders = tf.placeholder(tf.int32, shape=(None, 4))
         label_placeholders = [tf.placeholder(tf.float32, shape=(None, 1)) for i in range(num_relations)]
         corrupt_placeholder = tf.placeholder(tf.bool, shape=(1))
-        inference = ntn.inference(batch_placeholders, corrupt_placeholder, init_word_embeds, entity_to_wordvec, \
+        logits, my_labels = ntn.inference(batch_placeholders, corrupt_placeholder, init_word_embeds, entity_to_wordvec, \
                 num_entities, num_relations, slice_size, batch_size, True, label_placeholders)
-        eval_correct = ntn.eval(inference)
+        print('logits:', logits)
+        print('my_labels:', my_labels)
+        accuracy = ntn.eval(logits, my_labels)
         saver = tf.train.Saver()
+        # saver = tf.train.import_meta_graph(params.output_path + 'Wordnet90.sess.meta')
 
-        saver.restore(sess, params.output_path+'around100/Wordnet70.sess')
+        # saver.restore(sess, params.output_path+'around100/Wordnet70.sess')
+        # saver.restore(sess, params.output_path + 'Wordnet90.sess.data-00000-of-00001')
+        saver.restore(sess, params.output_path + 'Wordnet90.sess')
         #init = tf.initialize_all_variables()
         #sess.run(init)
-        print(do_eval(sess, eval_correct, batch_placeholders, label_placeholders, corrupt_placeholder, batches, labels, batch_size))
+        print(do_eval(sess, accuracy, batch_placeholders, label_placeholders, corrupt_placeholder, batches, batch_size))
 
-def do_eval(sess, eval_correct, batch_placeholders, label_placeholders, corrupt_placeholder, test_batches, test_labels, num_examples):
+def do_eval(sess, accuracy, batch_placeholders, label_placeholders, corrupt_placeholder, test_batches, num_examples):
     print("Starting do eval")
+    print('sess:', sess)
+    print('accuracy:', accuracy)
+    # print('my_labels:', my_labels)
+    print('batch_placeholders:', batch_placeholders)
+    print('label_placeholders:', label_placeholders)
+    print('corrupt_placeholder:', corrupt_placeholder)
+    # print('test_batches:', test_batches)
+    # print('labels:', labels)
+    print('num_examples:', num_examples)
     true_count = 0.
 
-    feed_dict = fill_feed_dict(test_batches, test_labels, params.train_both, batch_placeholders, label_placeholders, corrupt_placeholder)
-    #predictions,labels = sess.run(eval_correct, feed_dict)
-    predictions, labels = sess.run(eval_correct, feed_dict)
-    print(predictions)
-    for i in range(len(predictions[0])):
-        if predictions[0][i]>0 and labels[0][i]==1:
-            true_count +=1.0
-        elif predictions[0][i]<0 and labels[0][i]==-1:
-            true_count +=1.0
-    precision = float(true_count) / float(num_examples)
+    feed_dict = fill_feed_dict(test_batches, params.train_both, batch_placeholders, label_placeholders, corrupt_placeholder)
+    precision = sess.run([accuracy], feed_dict=feed_dict)
+    # print('predictions:', predictions)
+    # print(predictions)
+    # for i in range(len(logits)):
+    #     if predictions[i] > 0 and labels[i] == 1:
+    #         true_count += 1.0
+    #     elif predictions[i] < 0 and labels[i] == -1:
+    #         true_count += 1.0
+    # precision = float(true_count) / float(num_examples)
+    # correct_prediction = tf.equal(predictions, tf.argmax(my_labels, 1))
+    # precision = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+
     return precision
 
 def getThresholds():
